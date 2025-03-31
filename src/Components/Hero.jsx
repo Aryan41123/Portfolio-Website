1b6ad7af481d55818a6b5993be7aff181c2d8ede
Aryan <Aryan1284@gmail.com>
2025-03-31 12:38:10 +0530

    55

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
                     <div className="flex flex-col items-center lg:items-start">
                         <h1 className='pb-16 text-6xl font-normal tracking-tight lg:mt-16 lg:text-8xl'>Aryan</h1>
                         <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl text-transparent'>Full Stack Developer</span>
-                        <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>I am a passionate full stack developer with a knack for crafting robust and scalable web applications.I have honed my skills in front-end technologies like React as well as back-end technologies like Node.js,Express.js and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.</p>
+                        <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>I am a passionate full stack developer with a passion for crafting robust and scalable web applications.I have honed my skills in front-end technologies like React as well as back-end technologies like Node.js,Express.js and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.</p>
 
                     </div>
                 </div>
