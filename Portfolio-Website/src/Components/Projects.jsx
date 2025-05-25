f72dc866d2805367a047611d88ad5c53655f162e
Aryan <Aryan1284@gmail.com>
2025-05-25 22:00:55 +0530

    1299

--- Portfolio-Website/src/Components/Projects.jsx
+++ Portfolio-Website/src/Components/Projects.jsx
@@ -8,31 +8,28 @@ const Projects = () => {
             <div>
                 {PROJECTS.map((project, index) => (
                     <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-                        {/* Image Section */}
                         <div className="w-full lg:w-1/4">
-                            {project.link ? (
-                                <a href={project.link} target="_blank" rel="noopener noreferrer">
-                                    <img className='mb-6 rounded' src={project.image} width={150} height={150} alt={project.title} />
-                                </a>
-                            ) : (
-                                <img className='mb-6 rounded' src={project.image} width={150} height={150} alt={project.title} />
-                            )}
+                            <img className='mb-6 rounded' src={project.image} width={150} height={150} alt={project.title} />
                         </div>
 
-                        {/* Text Section */}
                         <div className="w-full max-w-xl lg:w-1/4">
-                            {project.link ? (
-                                <a href={project.link} target="_blank" rel="noopener noreferrer">
-                                    <h6 className='mb-2 font-semibold text-blue-400 hover:underline'>{project.title}</h6>
-                                </a>
-                            ) : (
-                                <h6 className='mb-2 font-semibold'>{project.title}</h6>
-                            )}
+                            <h6
+                                onClick={() => {
+                                    if (project.link) {
+                                        window.open(project.link, '_blank')
+                                    }
+                                }}
+                                className={`mb-2 font-semibold cursor-pointer ${project.link ? 'text-blue-400 hover:underline' : ''}`}
+                            >
+                                {project.title}
+                            </h6>
 
                             <p className='mb-4 text-neutral-400'>{project.description}</p>
 
                             {project.technologies.map((tech, techIndex) => (
-                                <span className='mr-2 rounded px-2 py-1 text-sm bg-neutral-800' key={techIndex}>{tech}</span>
+                                <span className='mr-2 rounded px-2 py-1 text-sm bg-neutral-800' key={techIndex}>
+                                    {tech}
+                                </span>
                             ))}
                         </div>
                     </div>
