25c8c6fae7b24fe0227bb134b135440be63eef13
Aryan <Aryan1284@gmail.com>
2026-04-22 13:06:53 +0530

    feat: add constants for hero, about, experience, projects, and contact data

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 import project1 from "../assets/projects/image.webp";
 import project2 from "../assets/projects/School-Logo-1.jpg";
-import project3 from "../assets/projects/project-3.png";
+import project3 from "../assets/projects/project-3.jpg";
 // import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications.I have honed my skills in front-end technologies like React as well as back-end technologies like Node.js,Express.js and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
